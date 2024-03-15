47025aa301446a3b54686b859e430575ec34ef9e
Vladimir Aguacunchi <marwladiagua2001@hotmail.com>
2024-03-14 21:38:35 -0500

    next config

--- apis/enagApi.ts
+++ apis/enagApi.ts
@@ -2,12 +2,6 @@ import axios from 'axios';
 
 const enagApi=axios.create({
     baseURL:'https://enag.vercel.app/api',
-    headers:{
-        ['Access-Control-Allow-Origin']:'*',
-        ['Access-Control-Allow-Methods']:'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        ['Access-Control-Allow-Headers']:'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        ['Access-Control-Allow-Credentials']:'true'
-    }
 })
 
 export default enagApi;
