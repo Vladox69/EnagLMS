04b38b69ba5ac54ff9f890c08eebe097fb7f2208
Vladimir Aguacunchi <marwladiagua2001@hotmail.com>
2024-08-19 15:58:38 -0500

    change logout

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,27 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { verify } from 'jsonwebtoken'
 import { serialize } from "cookie";
 
-export default async function logoutHandler(req: NextApiRequest, res: NextApiResponse) {
-    const { enag_session } = req.cookies
-    console.log(enag_session);
-    
-    if (!enag_session) {
-        return res.status(401).json({ error: 'Token not found' })
-    }
-    try {
-        verify(enag_session, 'secret')
-        const serealized = serialize('enag_session', '', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: -1,
-            path: '/'
-        })
-        res.setHeader('Set-Cookie',serealized)
-        return res.status(200).json('logout sucess')
-    } catch (error) {
-        return res.status(401).json({ error: 'Invalid token' })
-    }
+export default async function logoutHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const serialized = serialize("enag_session", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: -1,
+    path: "/",
+  });
 
+  res.setHeader("Set-Cookie", serialized);
+  return res.status(200).json({ message: "Logged out successfully" });
 }
