c471a9a42300a7ae39ef30bce9629e4568203617
Vladimir Aguacunchi <marwladiagua2001@hotmail.com>
2024-08-19 16:13:15 -0500

    cookie

--- pages/api/auth/logout.ts
+++ pages/api/auth/logout.ts
@@ -1,18 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { serialize } from "cookie";
 
-export default async function logoutHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const serialized = serialize("enag_session", "", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: -1,
-    path: "/",
-  });
+export default async function logoutHandler(req: NextApiRequest, res: NextApiResponse) {
+    const serialized = serialize('enag_session', '', {
+        httpOnly: true,
+        secure: true, // Asegura que la cookie se envíe solo sobre HTTPS
+        sameSite: 'strict',
+        maxAge: -1, // Usar -1 para eliminar la cookie inmediatamente
+        path: '/'
+    });
 
-  res.setHeader("Set-Cookie", serialized);
-  return res.status(200).json({ message: "Logged out successfully" });
+    res.setHeader('Set-Cookie', serialized);
+    res.status(200).json({ message: 'Logged out successfully' });
 }
